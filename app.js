0c97ea2f4ad775d8b362214e3a74f042f0cd0c1b
Muhammad Talal Aslam <146806220+ChaudharyTalal@users.noreply.github.com>
2025-11-17 16:45:25 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,28 @@
-// core modules
-const path = require("path");
-//external modules
-const express = require("express");
-//local Router
-const userRouter = require("./routes/userRouter");
-const {hostRouter} = require("./routes/hostRouter");
-// Get the project root directory directly from app.js
-const rootDir = __dirname;
-
+const express = require('express');
+const path = require('path')
 const app = express();
+// const bodyParser = require('body-parser'); // not needed: using built-in parsers
+
+// local modules
+const storeRouter = require('./routes/storeRouter');
+const { adminRouter } = require('./routes/adminRouter');
+const { notfound } = require('./controllers/404');
+
+// body parsing
+app.use(express.urlencoded())
+app.use(express.json())
+
+// static files (serve ./public)
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.set('view engine','ejs')
-app.set('views', path.join(rootDir, 'views'))
+app.set('views','views')
 
-// app.use((req, res, next) => {
-//     console.log(req.url, req.method);
-//     next();
-// });
+app.use('/',storeRouter)
+app.use('/admin',adminRouter)
 
-
-
-
-app.use(express.urlencoded());
-app.use(userRouter);
-app.use("/host", hostRouter);
-
-// use public folder files
-app.use(express.static(path.join(rootDir,'public')))
-
-app.use((req, res, next) => {
-    res.status(404).render('404',{pageTitle:"404 Page not found"})
-});
+app.use(notfound)
 
 // Export the app for serverless platforms (e.g., Vercel)
 module.exports = app;
+
